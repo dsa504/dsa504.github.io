2f5685704e4fb672c26c32e39c785dd98cd30205
Stephen Price <spprice@nola.gov>
2017-09-23 13:42:18 -0500

    refactor events page to es6

--- js/events.js
+++ js/events.js
@@ -1,66 +1,65 @@
-var mykey = 'AIzaSyAHAKS3oeDgnsknND2GVeCTiDgKqy7uMrw'; // sumthin like Gtg-rtZdsreUr_fLfhgPfgff
-var calendarid = 'nbq9d5kvi60fj6m34ml77a6hdc@group.calendar.google.com'; // looks like nbq9d5kvi60fj6m34ml77a6hdc@group.calendar.google.com
-
-var caldata = "";
-
-new Vue({
-    el: '#dsa-cal-app',
-    data: {
-        calevents: '',
-        errors:''
-    },
-    methods: {
-        getPrettyDate: function(uglydateTime){
-            prettyPattern = "dddd, MMMM Do YYYY, h:mm a";
-            
-            if(uglydateTime == undefined){
-                console.log("derp");
-            }
-            var theMoment = moment(uglydateTime);
-            // this is maybe the not most rational way to do this <_<
-            var day = theMoment.clone().format("dddd");
-            var month = theMoment.clone().format("MMMM");
-            var date = theMoment.clone().format("Do");
-            var year = theMoment.clone().format("YYYY");
-            var time = theMoment.clone().format("h:mm");
-            var ampm = theMoment.clone().format("a");
-            var output = [{day,month,date,year,time,ampm}];
-            return output;
-        }
-    },
-    mounted: function() {
-        var th = this;
-        var today = new Date();
-        $.ajax({
-            type: 'GET',
-            url: encodeURI('https://www.googleapis.com/calendar/v3/calendars/' + calendarid+ '/events?key=' + mykey +'&timeMin='+ today.toISOString()), //only get upcoming events
-            dataType: 'json',
-            success: function (response) {
-                var theEvents = response.items;
-                console.log(theEvents);
-
-                // all day events don't have a dateTime, so will screw up the chonological order- so if there's no dateTime, just use date
-                theEvents.forEach(function(event){
-                    if(event.start.dateTime == undefined){
-                        console.log("no dateTime for "+event.summary+", using date");
-                        event.theStart = event.start.date;
-                    } else {
-                        event.theStart = event.start.dateTime;
-                    }
-                });
-                
-                theEvents.sort(function(a,b){ // sort to make sure the events are in chron order
-                    a = new Date(a.theStart);
-                    b = new Date(b.theStart);
-                    return a<b ? -1 : a>b ? 1 : 0;}
-                );
-                
-                th.calevents = theEvents;
-            },
-            error: function (response) {
-                th.errors = "Derp. Something went wrong- if you're seeing this, please let us know on <a href='https://www.facebook.com/NewOrleansDSA/'>Facebook</a> or the <a href='https://twitter.com/neworleansdsa'>Twitters</a>!"; 
-            }
-        });
-    }
+(() => {
     
-})
+    const createVm = (config) => {
+        const _vm = {
+            el: '#dsa-cal-app',
+            data: {
+                calevents: '',
+                errors:''
+            },
+            methods: {
+                getPrettyDate: (uglydateTime) => {
+                    const prettyPattern = "dddd, MMMM Do YYYY, h:mm a";
+                    
+                    if(uglydateTime == undefined){
+                        console.log("derp");
+                    }
+                    const theMoment = moment(uglydateTime);
+                    // this is maybe the not most rational way to do this <_<
+                    const day = theMoment.clone().format("dddd");
+                    const month = theMoment.clone().format("MMMM");
+                    const date = theMoment.clone().format("Do");
+                    const year = theMoment.clone().format("YYYY");
+                    const time = theMoment.clone().format("h:mm");
+                    const ampm = theMoment.clone().format("a");
+                    const output = {day,month,date,year,time,ampm};
+                    return output;
+                }
+            },
+            mounted: () => {
+                const today = new Date();
+                // only get upcoming events:
+                const uri = `https://www.googleapis.com/calendar/v3/calendars/${config.calendarId}/events?key=${config.apiKey}&timeMin=${today.toISOString()}`;
+                try {
+                    const response = await fetch(uri);
+                }
+                catch (r) {
+                    vm.errors = "Derp. Something went wrong- if you're seeing this, please let us know on <a href='https://www.facebook.com/NewOrleansDSA/'>Facebook</a> or the <a href='https://twitter.com/neworleansdsa'>Twitters</a>!";
+                    return;
+                }
+        
+                const theEvents = (await response.json()).items;
+                
+                _vm.calevents = theEvents
+                    // all day events don't have a dateTime, so will screw up the chonological order- so if there's no dateTime, just use date
+                    .map((e) => {
+                        const theStart = (e.start.dateTime == undefined) ? e.start.date : e.start.dateTime;
+                        return {...e, theStart, prettyDate: _vm.getPrettyDate(e)};
+                    })
+                    // sort to make sure the events are in chron order
+                    .sort((a,b) => { 
+                        const a = new Date(a.theStart);
+                        const b = new Date(b.theStart);
+                        return a<b ? -1 : a>b ? 1 : 0;
+                    });
+            }
+        };
+
+        return _vm;
+    };
+    
+    const configResponse = await fetch("/calendarconfig.json");
+    const config = await configResponse.json();
+
+    new Vue(createVm(config));
+})();
