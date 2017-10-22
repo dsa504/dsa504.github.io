5ed2ab27cc0487757fa8339590326297ca9b51b8
Mark <mdarens@gmail.com>
2017-10-22 13:52:59 -0500

    Update devDependencies, specify webpack version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,29 +1,29 @@
 import path from "path";
 
 const config = {
-    context: __dirname,
-    entry: {
-        // index: "js/src/index.js",
-        events: "./js/src/events.js",
+	context: __dirname,
+	entry: {
+		// index: "js/src/index.js",
+		events: "./js/src/events.js",
 	},
 	module: {
 		rules: [
-		  	{
+			{
 				test: /\.js$/,
 				exclude: /(node_modules|bower_components)/,
 				use: {
-			  		loader: 'babel-loader',
-			  		options: {
+					loader: 'babel-loader',
+					options: {
 						presets: ['env']
-			  		}
+					}
 				}
-		  	}
+			}
 		]
 	},
-    output: {
-        filename: "[name].js",
-        path: `${__dirname}/js/lib` 
-    },
+	output: {
+		filename: "[name].js",
+		path: `${__dirname}/js/lib`
+	},
 };
 
 export default config;
