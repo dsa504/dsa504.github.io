9a90a1c63471347eb0e27c7c90762d06b57c7da7
Mark <mdarens@gmail.com>
2017-12-14 15:27:22 -0600

    how bout just getting the api to do it for us huh

--- js/src/events.js
+++ js/src/events.js
@@ -1,20 +1,20 @@
 //import Vue from "vue";
-// import moment from "moment";
-import {} from "babel-polyfill";
+//import moment from "moment";
+import { } from "babel-polyfill";
 
 (async () => {
-    
+
     const getPrettyDate = (uglydateTime) => {
         const theMoment = moment(uglydateTime);
-        
+
         const day = theMoment.format("dddd");
         const month = theMoment.format("MMMM");
         const date = theMoment.format("Do");
         const year = theMoment.format("YYYY");
         const time = theMoment.format("h:mm");
         const ampm = theMoment.format("a");
-        const output = {day,month,date,year,time,ampm};
-        
+        const output = { day, month, date, year, time, ampm };
+
         return output;
     };
 
@@ -26,14 +26,23 @@ import {} from "babel-polyfill";
                 showErrors: false,
             },
             methods: {
-                
+
             },
             //showErrors:false,
             mounted: async () => {
                 const today = moment(new Date());
+                const timeMax = moment(new Date()).add(3, "month");
                 // only get upcoming events:
-                const uri = `https://www.googleapis.com/calendar/v3/calendars/${config.calendarId}/events?key=${config.apiKey}&timeMin=${today.toISOString()}`;
-                
+                const uri = `https://www.googleapis.com/calendar/v3/calendars/${
+                    config.calendarId
+                    }/events?key=${
+                    config.apiKey
+                    }&timeMin=${
+                    today.toISOString()
+                    }&timeMax=${
+                    timeMax.toISOString()
+                    }&singleEvents=true&orderBy=startTime`;
+
                 let response;
                 try {
                     response = await fetch(uri);
@@ -42,58 +51,58 @@ import {} from "babel-polyfill";
                     _vm.showErrors = true;
                     return;
                 }
-        
+
                 if (!response.ok) {
-                    return _vm.showErrors = true 
+                    return _vm.showErrors = true
                 }
 
                 const theEvents = (await response.json()).items;
-                
+
                 _vm.data.calEvents = theEvents
                     // catch cancelled events
-                    .filter(e => e.start !== undefined) 
+                    .filter(e => e.start !== undefined)
                     // all day events don't have a dateTime, so will screw up the chonological order - 
                     // so if there's no dateTime, just use date
                     .map((e) => {
                         const hasStartTime = e.start.dateTime !== undefined;
                         const startTime = (hasStartTime) ? e.start.dateTime : e.start.date;
-                        
-                        const hasEndTime = e.end.dateTime !== undefined 
+
+                        const hasEndTime = e.end.dateTime !== undefined
                         const endTime = (hasEndTime) ? e.end.dateTime : e.end.date;
-                        
+
                         const prettyStartTime = getPrettyDate(startTime);
                         const prettyEndTime = getPrettyDate(endTime);
 
                         return {
-                            ...e, 
-                            startTime, 
-                            endTime, 
-                            hasStartTime, 
-                            hasEndTime, 
-                            prettyStartTime, 
-                            prettyEndTime, 
+                            ...e,
+                            startTime,
+                            endTime,
+                            hasStartTime,
+                            hasEndTime,
+                            prettyStartTime,
+                            prettyEndTime,
                         };
                     })
                     // catch events before today
                     .filter(e => !moment(e.startTime).isBefore(today))
                     // sort to make sure the events are in chron order
-                    .sort((x,y) => { 
+                    .sort((x, y) => {
                         const a = new Date(x.startTime);
                         const b = new Date(y.startTime);
-                        return a<b ? -1 : a>b ? 1 : 0;
+                        return a < b ? -1 : a > b ? 1 : 0;
                     });
             }
         };
 
         return _vm;
     };
-    
+
     let configResponse
-    
+
     try {
         configResponse = await fetch("/calendarconfig.json");
     }
-    catch(e){
+    catch (e) {
         console.log(e);
     }
     const config = await configResponse.json();
